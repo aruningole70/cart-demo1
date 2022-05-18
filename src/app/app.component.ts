019cd1a09d16ebfe0af4e8b86ac7c3a8af1606db
Arun Ingole <Arun.Ingole@ezest.local>
2022-05-18 13:21:39 +0530

    all files updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ezst';
+  isMenuOpen : boolean = false;
+  constructor(){
+
+  }
+  openCloseMobileMenu(){
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  // @HostListener('window:resize', ['$event'])
+  // onResize(event: any) {
+  //  console.log(window.innerWidth);
+  // //  this.isMenuOpen = !this.isMenuOpen;
+  //  let element = document.getElementById("ifNotMobile");
+  // //  element?.classList.add("display-header");
+   
+  // //  console.log(element?.classList.contains("display-header"));
+   
+  // }
 }
