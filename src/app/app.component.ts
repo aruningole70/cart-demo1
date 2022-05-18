05ab60920eb33eee6b844954cdc6e31f4021904d
Arun Ingole <Arun.Ingole@ezest.local>
2022-05-19 01:34:17 +0530

    added dynamic products functoianlity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,34 @@ import { Component, HostListener } from '@angular/core';
 export class AppComponent {
   title = 'ezst';
   isMenuOpen : boolean = false;
+  // banner1:any={}
   constructor(){
 
   }
   openCloseMobileMenu(){
     this.isMenuOpen = !this.isMenuOpen;
   }
+  banner1= [
+    {image:'assets/bottle1.png', name:'Acne Cream', details:'All Natural Ingredients', price:24.99},
+    {image:'assets/bottle2.png', name:'Vitiligio Solution', details:'All Natural Ingredients', price:24.99},
+    {image:'assets/bottle3.png', name:'Ezcema Cream', details:'All Natural Ingredients', price:24.99},
+    {image:'assets/bottle.png', name:'Sunburn Ointment', details:'All Natural Ingredients', price:24.99}];
+ banner2= [
+      {image:'assets/bottle.png', name:'Psoriasis Cream', details:'All Natural Ingredients', price:24.99},
+      {image:'assets/bottle3.png', name:'Ezcema Cream', details:'All Natural Ingredients', price:24.99},
+      {image:'assets/bottle4.png', name:'Sunburn Ointment', details:'All Natural Ingredients', price:24.99},
+      {image:'assets/Balm.png', name:'Vitiligio Balm', details:'All Natural Ingredients', price:24.99},
+      {image:'assets/bottle2.png', name:'Vitiligio Solution', details:'All Natural Ingredients', price:24.99},
+      {image:'assets/bottle1.png', name:'Acne Cream', details:'All Natural Ingredients', price:24.99},
+      {image:'assets/cream.png', name:'Acne Cream', details:'All Natural Ingredients', price:24.99},
+      {image:'assets/dropper.png', name:'Ezcema Oil', details:'All Natural Ingredients', price:24.99}];
+   banner3= [
+        {image:'assets/bottle.png', name:'Acne Cream', details:'All Natural Ingredients', price:24.99},
+        {image:'assets/bottle3.png', name:'Vitiligio Solution', details:'All Natural Ingredients', price:24.99},
+        {image:'assets/two-bottls.png', name:'Ezcema Cream', details:'All Natural Ingredients', price:24.99},
+        {image:'assets/pink-cream.png', name:'Sunburn Ointment', details:'All Natural Ingredients', price:24.99}];
+    
+};
   // @HostListener('window:resize', ['$event'])
   // onResize(event: any) {
   //  console.log(window.innerWidth);
@@ -24,4 +46,4 @@ export class AppComponent {
   // //  console.log(element?.classList.contains("display-header"));
    
   // }
-}
+
